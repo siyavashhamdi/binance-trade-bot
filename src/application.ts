ad2c60e6d4a5c9330ee68f314ac8b4c63da4c0b9
Siyavash <siyavashhamdi@gmail.com>
2021-08-05 16:45:18 +0430

    Fixed everything hitherto

--- src/application.ts
+++ src/application.ts
@@ -1,25 +1,16 @@
 import { TradeInfo } from './helper/trade';
 import utils from './helper/utils';
-import { std } from 'mathjs';
+import { std, mean } from 'mathjs';
 
 export async function bootstrap() {
     const tradeInfoEthBtc = new TradeInfo('ETH', 'BTC', { apiKey: process.env.BNC_API_KEY, apiSecret: process.env.BNC_API_SECRET });
 
     setInterval(async () => {
         try {
-            // await tradeInfoEthBtc.orderPlanA({
-            //     priceToBuy: 0.002,
-            //     desiredProfitPercentage: 0.05,
-            // })
-
-            const stdx = std([
-                Math.abs(0.068751 - 0.068628),
-                Math.abs(0.068757 - 0.068637),
-                Math.abs(0.068684 - 0.068763),
-                Math.abs(0.068739 - 0.068630),
-                Math.abs(0.069237 - 0.068721),
-            ]);
-            utils.log(stdx);
+            await tradeInfoEthBtc.orderPlanA({
+                priceToBuy: 0.002,
+                desiredProfitPercentage: 0.05,
+            })
 
         } catch (err) {
             utils.log(JSON.stringify({ err }));
