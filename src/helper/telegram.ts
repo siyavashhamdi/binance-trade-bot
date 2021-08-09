4e2c009529d22cd1ff1134e74a9444d5639aaab1
Siyavash <siyavashhamdi@gmail.com>
2021-08-10 02:43:20 +0430

    Get openOrders by Telegram

--- src/helper/telegram.ts
+++ src/helper/telegram.ts
@@ -38,9 +38,9 @@ export class Telegram {
 
                 case TelegramCommands.openOrders: {
                     this.tradeInfo?.getOpenOrders().then((openOrders) => {
-                        const resp = `Number of open orders: ${ openOrders.length }
-Low price is: \`${ openOrders.reduce((prev: any, curr: any) => prev.price < curr.price ? prev : curr) } \`
-High price is: \`${ openOrders.reduce((prev: any, curr: any) => prev.price > curr.price ? prev : curr) } \``
+                        const resp = `Number of open orders: \`${ openOrders.length }\`
+Low price: \`${ openOrders.reduce((prev: any, curr: any) => prev.price < curr.price ? prev : curr) } \`
+High price: \`${ openOrders.reduce((prev: any, curr: any) => prev.price > curr.price ? prev : curr) } \``
 
                         this.sendMessage(chatId, resp);
                     });
